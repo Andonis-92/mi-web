2e49b8cdabdc2ea954672860f907241080eb7530
Andonis-92 <adomingo1992@hotmail.com>
2026-06-17 11:49:23 +0200

    Update brodwei_redes3.js

--- configuracion/brodwei_redes3.js
+++ configuracion/brodwei_redes3.js
@@ -1,10 +1,10 @@
 /*USER SETTING*/
 var SOCIAL_LINKS = new Array(
-/*facebook*/'https://www.facebook.com/facebrodwei/',/*facebook*/
-/*youtube*/'https://www.youtube.com/channel/UCU_nCLG8XiIo9i_st8LZCJA',/*youtube*/
-/*tiktok*/'https://www.tiktok.com/@brodwei_',/*tiktok*/
-/*instagram*/'https://www.instagram.com/brodwei_/?hl=es',/*instagram*/
 /*spotify*/'https://open.spotify.com/artist/4Jf86IZAzXn1UDa7laE9yS',/*spotify*/
+/*instagram*/'https://www.instagram.com/brodwei_/?hl=es',/*instagram*/
+/*tiktok*/'https://www.tiktok.com/@brodwei_',/*tiktok*/
+/*youtube*/'https://www.youtube.com/channel/UCU_nCLG8XiIo9i_st8LZCJA',/*youtube*/	
+/*facebook*/'https://www.facebook.com/facebrodwei/',/*facebook*/
 /*google-plus*/'',/*google*/
 /*behance*/'',/*behance*/
 /*linkedin*/'',/*linkedin*/
